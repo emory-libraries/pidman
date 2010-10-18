683148061d8d1c640a206a57c6489da0140ababb
rsutton <rsutton@79750155-2c60-425a-961e-bef42b9dd43f>
2010-10-18 17:24:40 +0000

    release branch 0.9.x - REST api, sphinx docs

---
+++ pidman/branches/release_0.9.x/docs/api_list.rst
@@ -0,0 +1,100 @@
+Pidman API Documentation
+========================
+
+The following code is generated from introspection of the code comments.
+
+pid
+---
+
+.. automodule:: pidman.pid
+   :members:
+
+models
+^^^^^^
+
+.. automodule:: pidman.pid.models
+   :members:
+
+Utility Methods for ARKs
+^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. automodule:: pidman.pid.ark_utils
+   :members:
+
+noid functionality
+^^^^^^^^^^^^^^^^^^
+
+.. automodule:: pidman.pid.noid
+   :members:
+
+admin
+^^^^^
+
+.. automodule:: pidman.pid.admin
+   :members:
+
+pidauth
+-------
+
+.. automodule:: pidman.pidauth
+   :members:
+
+models
+^^^^^^
+
+.. automodule:: pidman.pidauth.models
+   :members:
+
+resolver
+--------
+
+.. automodule:: pidman.resolver
+   :members:
+   
+views
+^^^^^
+
+.. automodule:: pidman.resolver.views
+   :members:
+
+rest_api
+--------
+
+This module provides REST API access for CRUD calls on :class:`~pidman.pid.models.Pid`,
+:class:`~pidman.pid.models.Domain`, and :class:`~pidman.pid.models.Target` objects.
+
+.. automodule:: pidman.rest_api
+   :members:
+
+decorators
+^^^^^^^^^^
+
+.. automodule:: pidman.rest_api.decorators
+   :members:
+
+models
+^^^^^^
+
+.. automodule:: pidman.rest_api.models
+   :members:
+
+views
+^^^^^
+
+.. automodule:: pidman.rest_api.views
+   :members:
+
+soap_api
+--------
+
+.. automodule:: pidman.soap_api
+   :members:
+
+views
+^^^^^
+
+.. automodule:: pidman.soap_api.views
+   :members:
+
+.. autoclass:: pidman.soap_api.views.PersistentIdentifierService
+    :members:
---
+++ pidman/branches/release_0.9.x/pidman/resolver/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render_to_response
+from pidman.pid.models import Target, Pid
+from pidman.pid.ark_utils import normalize_ark
+
+def resolve_purl(request, noid):
+    return resolve_pid(noid)
+
+def resolve_ark(request, naan, noid, qual=''):
+    '''Resolve an ARK or qualified ARK to the appropriate target URL.
+
+    If there are no qualifiers and the URL ends with single question mark,
+    returns minimal metadata about the ARK.  If the URL ends with two question
+    marks, returns the commitment statement for the requested ARK.  See
+    :meth:`ark_metadata` for more information.
+    '''
+    noid = normalize_ark(noid)
+    if qual:
+        qual = normalize_ark(qual)
+
+    full_path = request.get_full_path()
+    # if last letter is ? then metadata or preservation commitment should be displayed
+    # NOTE: REQUEST_URI is *ONLY* available when running under mod_wsgi,
+    # and full path only includes ? if there is a query string. 
+    # See README file for more details.
+    if qual == '' and full_path[-1] == "?" or ('REQUEST_URI' in request.META.keys()
+        and request.META['REQUEST_URI'][-1] == "?"):
+        return ark_metadata(request, noid)
+    return resolve_pid(noid, qual)
+
+def resolve_pid(noid, qual=''):
+    '''Common functionality for resolving PURLs and ARKs.
+    
+    Retrive the :class:`Target` requested, prefix the :class:`Proxy` transform
+    to the target URL if a proxy is defined, and then redirect to the target URL.
+    '''
+    t = get_object_or_404(Target, noid__exact=noid, qualify=qual, active=True)
+    url = t.uri
+    if (t.proxy):
+        url = t.proxy.transform + url
+    return HttpResponseRedirect(url)
+
+# ARK spec 5.1.2 generic description service
+# ARK spec 5.1.1 generic policy service
+def ark_metadata(request, noid):
+    '''Display minimal ARK metadata or preservation commitment statement in a
+    simple, plain-text format.
+
+    See ARK specification sections 5.1.1 and 5.1.2 for more information.'''
+    pid = get_object_or_404(Pid, pid__exact=noid)
+    full_path = request.get_full_path()
+    if full_path[-2:] == "??":
+        policy = pid.get_policy()
+    else:
+        policy = None
+        
+    return render_to_response("resolver/about",
+                              {"pid": pid, "policy" : policy},
+                              mimetype="text/plain")
+
+
