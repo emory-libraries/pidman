982dc662a8127de4d22e878189457282b44e35ba
rlskoeser <rebecca.s.koeser@emory.edu>
2016-03-18 16:00:34 -0400

    Set version to 0.10; add @alexBLR to contributors

--- pidman/__init__.py
+++ pidman/__init__.py
@@ -1,11 +1,12 @@
-# Project Version and Author Information
+ # Project Version and Author Information
 __author__ = "EUL Systems"
-__copyright__ = "Copyright 2010, Emory University General Library"
-__credits__ = ["Rebecca Koeser", "Ben Ranker", "Alex Thomas", "Scott Turnbull"]
+__copyright__ = "Copyright 2010, 2016. Emory University Library and IT Services"
+__credits__ = ["Rebecca Koeser", "Ben Ranker", "Alex Thomas", "Scott Turnbull",
+    "Alex Zotov"]
 __email__ = "libsys-dev@listserv.cc.emory.edu"
 
 # Version Info, parsed below for actual version number.
-__version_info__ = (0, 9, 0)
+__version_info__ = (0, 10, 0)
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join([ str(i) for i in __version_info__[:-1] ])
