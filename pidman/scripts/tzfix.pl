1c96e256f979fe5526fa66544a9eca086f12e493
rlskoeser <rebecca.s.koeser@emory.edu>
2016-05-25 15:56:13 -0400

    Remove alternate pg migration script based on Mark's shell scripts

--- pidman/scripts/tzfix.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-while(chomp($line=<>)) {
-   # $line=~s/(\.[0-9]+?)-[0-9]{2}/$1/g;
-   $line=~s/\.[0-9]+?-[0-9]{2}//g;
-  # $line=~s/\((.+?),/\( $1 ,/g;
-   $line=~s/\r//g;
-   print "$line\n";
-}
